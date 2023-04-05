fc0fd64234faa2a942f8e9e268456a305a9aaf43
Rick de Rooij <rick.de.rooij@contezza.nl>
2023-04-05 13:20:50 +0200

    🚀 complete tests

--- src/test/java/nl/contezza/drc/tests/UploadTest.java
+++ src/test/java/nl/contezza/drc/tests/UploadTest.java
@@ -7,10 +7,10 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import lombok.extern.log4j.Log4j2;
 import nl.contezza.drc.dataprovider.DRCDataProvider;
 import nl.contezza.drc.rest.RestTest;
 import nl.contezza.drc.service.AuthService;
@@ -19,7 +19,7 @@ import nl.contezza.drc.service.EIOService;
 import nl.contezza.drc.service.UploadService;
 import nl.contezza.drc.service.ZTCService;
 
-@Log4j2
+//@Log4j2
 public class UploadTest extends RestTest {
 
 	/**
@@ -47,7 +47,7 @@ public class UploadTest extends RestTest {
 	 * "https://github.com/VNG-Realisatie/documenten-api/blob/1.3.0/src/drc/tests/test_upload.py#L43">python
 	 * code</a>}.
 	 */
-	// @Test(groups = "Upload")
+	@Test(groups = "Upload")
 	public void test_create_eio() {
 		EIOService eioService = new EIOService();
 
@@ -69,7 +69,7 @@ public class UploadTest extends RestTest {
 	 * "https://github.com/VNG-Realisatie/documenten-api/blob/1.3.0/src/drc/tests/test_upload.py#L92">python
 	 * code</a>}.
 	 */
-	// @Test(groups = "Upload")
+	@Test(groups = "Upload")
 	public void test_create_without_file() {
 		EIOService eioService = new EIOService();
 
@@ -92,7 +92,7 @@ public class UploadTest extends RestTest {
 	 * "https://github.com/VNG-Realisatie/documenten-api/blob/1.3.0/src/drc/tests/test_upload.py#L132">python
 	 * code</a>}.
 	 */
-	// @Test(groups = "Upload")
+	@Test(groups = "Upload")
 	public void test_create_empty_file() {
 		EIOService eioService = new EIOService();
 
@@ -115,7 +115,7 @@ public class UploadTest extends RestTest {
 	 * "https://github.com/VNG-Realisatie/documenten-api/blob/1.3.0/src/drc/tests/test_upload.py#L180">python
 	 * code</a>}.
 	 */
-	// @Test(groups = "Upload")
+	@Test(groups = "Upload")
 	public void test_create_without_size() {
 		EIOService eioService = new EIOService();
 
@@ -132,7 +132,7 @@ public class UploadTest extends RestTest {
 	 * "https://github.com/VNG-Realisatie/documenten-api/blob/1.3.0/src/drc/tests/test_upload.py#L217">python
 	 * code</a>}.
 	 */
-	// @Test(groups = "Upload")
+	@Test(groups = "Upload")
 	public void test_update_eio_metadata() {
 		EIOService eioService = new EIOService();
 
@@ -157,7 +157,7 @@ public class UploadTest extends RestTest {
 	 * "https://github.com/VNG-Realisatie/documenten-api/blob/1.3.0/src/drc/tests/test_upload.py#L258">python
 	 * code</a>}.
 	 */
-	// @Test(groups = "Upload")
+	@Test(groups = "Upload")
 	public void test_update_eio_file() {
 		EIOService eioService = new EIOService();
 
@@ -183,7 +183,7 @@ public class UploadTest extends RestTest {
 	 * "https://github.com/VNG-Realisatie/documenten-api/blob/1.3.0/src/drc/tests/test_upload.py#L304">python
 	 * code</a>}.
 	 */
-	// @Test(groups = "Upload")
+	@Test(groups = "Upload")
 	public void test_update_eio_file_set_empty() {
 		EIOService eioService = new EIOService();
 
@@ -209,7 +209,7 @@ public class UploadTest extends RestTest {
 	 * "https://github.com/VNG-Realisatie/documenten-api/blob/1.3.0/src/drc/tests/test_upload.py#L341">python
 	 * code</a>}.
 	 */
-	// @Test(groups = "Upload")
+	@Test(groups = "Upload")
 	public void test_update_eio_only_size() {
 		EIOService eioService = new EIOService();
 
@@ -232,7 +232,7 @@ public class UploadTest extends RestTest {
 	 * "https://github.com/VNG-Realisatie/documenten-api/blob/1.3.0/src/drc/tests/test_upload.py#L371">python
 	 * code</a>}.
 	 */
-	// @Test(groups = "Upload")
+	@Test(groups = "Upload")
 	public void test_update_eio_only_file_without_size() {
 		EIOService eioService = new EIOService();
 
@@ -255,7 +255,7 @@ public class UploadTest extends RestTest {
 	 * "https://github.com/VNG-Realisatie/documenten-api/blob/1.3.0/src/drc/tests/test_upload.py#L606">python
 	 * code</a>}.
 	 */
-	// @Test(groups = "Upload")
+	@Test(groups = "Upload")
 	public void test_upload_part_wrong_size() {
 		UploadService uploadService = new UploadService();
 		EIOService eioService = new EIOService();
@@ -282,7 +282,7 @@ public class UploadTest extends RestTest {
 	 * "https://github.com/VNG-Realisatie/documenten-api/blob/1.3.0/src/drc/tests/test_upload.py#L672">python
 	 * code</a>}.
 	 */
-	// @Test(groups = "Upload")
+	@Test(groups = "Upload")
 	public void test_unlock_without_uploading() {
 		AuthService authService = new AuthService();
 
@@ -322,39 +322,139 @@ public class UploadTest extends RestTest {
 		Assert.assertEquals(res.body().path("invalidParams[0].code"), "file-size");
 	}
 
-	// TODO: create test
-	// @Test(groups = "Upload")
-	public void test_unlock_not_finish_upload() {
+	/**
+	 * See {@link <a href=
+	 * "https://github.com/VNG-Realisatie/documenten-api/blob/1.3.0/src/drc/tests/test_upload.py#L735">python
+	 * code</a>}.
+	 */
+	@Test(groups = "Upload")
+	public void test_unlock_not_finish_upload_force() {
+		AuthService authService = new AuthService();
 
+		JSONArray scopes = new JSONArray().put("documenten.lezen").put("documenten.aanmaken")
+				.put("documenten.bijwerken").put("documenten.lock").put("documenten.geforceerd-unlock");
+		String maxVertrouwelijkheid = "zeer_geheim";
+
+		JSONObject comp = new JSONObject();
+		comp.put("component", "drc");
+		comp.put("scopes", scopes);
+		comp.put("informatieobjecttype", informatieobjecttypeUrl);
+		comp.put("maxVertrouwelijkheidaanduiding", maxVertrouwelijkheid);
+
+		authService.updateReadOnlyClientScope(scopes, new JSONArray().put(comp), false);
+
+		wait(2000);
+
+		EIOService eioService = new EIOService();
+
+		JSONObject jsonObject = new JSONObject(DRCDataProvider.testCreate(informatieobjecttypeUrl));
+		jsonObject.put("inhoud", JSONObject.NULL);
+		jsonObject.put("bestandsomvang", "some content for file".getBytes().length);
+
+		Response res = eioService.testCreate(DRCRequestSpecification.getReadonly(), jsonObject);
+
+		String eioUrl = res.body().path("url");
+		String uploadUrl = res.body().path("bestandsdelen[0].url");
+		String lock = res.body().path("lock");
+
+		Assert.assertNotNull(uploadUrl);
+		Assert.assertNotNull(lock);
+
+		res = eioService.unlock(DRCRequestSpecification.getReadonly(), eioUrl, lock);
+
+		JsonPath json = new JsonPath(eioService.getEIO(DRCRequestSpecification.getReadonly(), eioUrl, null).asString());
+
+		// When no parts are uploaded, apparently bestandsdelen will be present
+		Assert.assertEquals(res.getStatusCode(), 204);
+		Assert.assertEquals(json.getList("bestandsdelen").size(), 1);
+		Assert.assertNull(json.get("inhoud"));
 	}
 
-	// TODO: create test
-	// @Test(groups = "Upload")
+	/**
+	 * See {@link <a href=
+	 * "https://github.com/VNG-Realisatie/documenten-api/blob/1.3.0/src/drc/tests/test_upload.py#L782">python
+	 * code</a>}.
+	 */
+	@Test(groups = "Upload")
 	public void test_update_metadata_without_upload() {
 
+		EIOService eioService = new EIOService();
+
+		JSONObject jsonObject = new JSONObject(DRCDataProvider.testCreate(informatieobjecttypeUrl));
+		jsonObject.put("inhoud", JSONObject.NULL);
+		jsonObject.put("bestandsomvang", "some content for file".getBytes().length + 1);
+
+		Response res = eioService.testCreate(jsonObject);
+
+		JSONObject body = new JSONObject();
+		body.put("beschrijving", "beschrijving2");
+		body.put("lock", res.body().path("lock").toString());
+
+		res = eioService.partialUpdate(res.body().path("url"), body);
+
+		JsonPath json = new JsonPath(res.asString());
+
+		Assert.assertEquals(res.getStatusCode(), 200);
+		Assert.assertEquals(json.getList("bestandsdelen").size(), 1);
+		Assert.assertEquals(json.getString("beschrijving"), "beschrijving2");
+		Assert.assertNull(res.getBody().path("inhoud"));
 	}
 
-	// TODO: create test
-	// @Test(groups = "Upload")
-	public void test_update_metadata_after_unfinished_upload() {
-
-	}
-
-	// TODO: create test
-	// @Test(groups = "Upload")
-	public void test_update_metadata_set_size() {
-
-	}
-
-	// TODO: create test
-	// @Test(groups = "Upload")
+	/**
+	 * See {@link <a href=
+	 * "https://github.com/VNG-Realisatie/documenten-api/blob/1.3.0/src/drc/tests/test_upload.py#L889">python
+	 * code</a>}.
+	 */
+	@Test(groups = "Upload")
 	public void test_update_metadata_set_size_zero() {
+		EIOService eioService = new EIOService();
 
+		JSONObject jsonObject = new JSONObject(DRCDataProvider.testCreate(informatieobjecttypeUrl));
+		jsonObject.put("inhoud", JSONObject.NULL);
+		jsonObject.put("bestandsomvang", "some content for file".getBytes().length);
+
+		Response res = eioService.testCreate(jsonObject);
+
+		String lock = res.body().path("lock");
+
+		JSONObject body = new JSONObject();
+		body.put("bestandsomvang", 0);
+		body.put("lock", lock);
+
+		res = eioService.partialUpdate(res.body().path("url"), body);
+		JsonPath json = new JsonPath(res.asString());
+
+		Assert.assertEquals(res.getStatusCode(), 200);
+		Assert.assertEquals(json.getList("bestandsdelen").size(), 0);
 	}
 
-	// TODO: create test
-	// @Test(groups = "Upload")
+	/**
+	 * See {@link <a href=
+	 * "https://github.com/VNG-Realisatie/documenten-api/blob/1.3.0/src/drc/tests/test_upload.py#L924">python
+	 * code</a>}.
+	 */
+	@Test(groups = "Upload")
 	public void test_update_metadata_set_size_null() {
+		EIOService eioService = new EIOService();
 
+		JSONObject jsonObject = new JSONObject(DRCDataProvider.testCreate(informatieobjecttypeUrl));
+		jsonObject.put("inhoud", JSONObject.NULL);
+		jsonObject.put("bestandsomvang", "some content for file".getBytes().length);
+
+		Response res = eioService.testCreate(jsonObject);
+
+		String lock = res.body().path("lock");
+
+		JSONObject body = new JSONObject();
+		body.put("bestandsomvang", JSONObject.NULL);
+		body.put("lock", lock);
+
+		res = eioService.partialUpdate(res.body().path("url"), body);
+		JsonPath json = new JsonPath(res.asString());
+
+		Assert.assertEquals(res.getStatusCode(), 200);
+		Assert.assertEquals(json.getList("bestandsdelen").size(), 0);
+		Assert.assertNull(json.get("bestandsomvang"));
+		Assert.assertNull(json.get("inhoud"));
 	}
 }
